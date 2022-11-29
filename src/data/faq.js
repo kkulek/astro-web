e952dce0197b813d6fc4f5b90a8fea828cd39078
kkulek <106318909+kkulek@users.noreply.github.com>
2022-11-29 14:17:16 +0100

    added faq data

---
+++ src/data/faq.js
@@ -0,0 +1,22 @@
+export const faqsArr = [
+	{
+		question: "What are some random questions to ask?",
+		answer: "That's exactly the reason we created this random question generator. There are hundreds of random questions to choose from so you're able to find the perfect random question.",
+	},
+	{
+		question: "Do you include common questions?",
+		answer: "This generator doesn't include most common questions. The thought is that you can come up with common questions on your own so most of the questions in this generator.",
+	},
+	{
+		question: "Can I use this for 21 questions?",
+		answer: "Yes! there are two ways that you can use this question generator depending on what you're after. You can indicate that you want 21 questions generated.",
+	},
+	{
+		question: "Are these questions for girls or for boys?",
+		answer: "The questions in this generator are gender neutral and can be used to ask either male of females (or any other gender the person identifies with).",
+	},
+	{
+		question: "What do you wish you had more talent doing?",
+		answer: "If you've been searching for a way to get random questions, you've landed on the correct webpage. We created the Random Question Generator to ask you as many random questions as your heart desires.",
+	},
+];
