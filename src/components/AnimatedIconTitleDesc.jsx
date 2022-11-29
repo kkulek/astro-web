6f301c56434e1f0e4d2da0949bb1f694aa688214
kkulek <106318909+kkulek@users.noreply.github.com>
2022-11-29 09:41:48 +0100

    refactored props

--- src/components/AnimatedIconTitleDesc.jsx
+++ src/components/AnimatedIconTitleDesc.jsx
@@ -2,13 +2,8 @@ import React from "react";
 import { motion } from "framer-motion";
 import Icon from "./Icon";
 
-export default function AnimatedIconTitleDesc({
-	title,
-	description,
-	icon,
-	iconColor,
-	index,
-}) {
+export default function AnimatedIconTitleDesc({ item, index }) {
+	const { title, description, icon, iconColor } = item;
 
 	return (
 		<motion.div
@@ -23,7 +18,7 @@ export default function AnimatedIconTitleDesc({
 					class="text-xl font-semibold text-black"
 					initial={{ opacity: 0, scale: 0.5 }}
 					animate={{ opacity: 1, scale: 1 }}
-					transition={{ duration: 0.5, delay: (0.15 * index) }}
+					transition={{ duration: 0.5, delay: 0.15 * index }}
 				>
 					{title}
 				</motion.h3>
@@ -31,7 +26,7 @@ export default function AnimatedIconTitleDesc({
 					class="mt-3 text-base text-gray-600"
 					initial={{ opacity: 0, scale: 0.5 }}
 					animate={{ opacity: 1, scale: 1 }}
-					transition={{ duration: 0.5, delay: (0.15 * index) }}
+					transition={{ duration: 0.5, delay: 0.15 * index }}
 				>
 					{description}
 				</motion.p>
