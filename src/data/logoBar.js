8e77fce20d19861498c0fb8977d021e72137dc30
kkulek <106318909+kkulek@users.noreply.github.com>
2022-11-30 11:15:26 +0100

    moved logotypes array to a separate file

---
+++ src/data/logoBar.js
@@ -0,0 +1,58 @@
+export const images = [
+	{
+		logo: "1.webp",
+		name: "Autospeed",
+	},
+	{
+		logo: "2.webp",
+		name: "Beauty Box",
+	},
+	{
+		logo: "3.webp",
+		name: "Pete's Blinds",
+	},
+	{
+		logo: "4.webp",
+		name: "Croft's Accountants",
+	},
+	{
+		logo: "5.webp",
+		name: "Dorfus",
+	},
+	{
+		logo: "6.webp",
+		name: "Greener",
+	},
+	{
+		logo: "7.webp",
+		name: "Coworks",
+	},
+	{
+		logo: "8.webp",
+		name: "MakeLess",
+	},
+	{
+		logo: "9.webp",
+		name: "Sass Today",
+	},
+	{
+		logo: "10.webp",
+		name: "Askimat",
+	},
+	{
+		logo: "11.webp",
+		name: "Martino",
+	},
+	{
+		logo: "12.webp",
+		name: "Rocker",
+	},
+	{
+		logo: "13.webp",
+		name: "Paperz",
+	},
+	{
+		logo: "14.webp",
+		name: "Square",
+	},
+];
